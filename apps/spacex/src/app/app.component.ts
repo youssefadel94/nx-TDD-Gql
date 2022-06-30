209b02109140d30158e79823dd5c8d6b3e1735de
Youssef adel <darkmetalxdragon@hotmail.com>
2022-06-30 22:20:11 +0200

    initial commit

---
+++ apps/spacex/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'yadel-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'spacex';
+}
